67abb37ca87e34e640a0b54b5d90f60277b69b0e
vimozgon <mozgonov@gmail.com>
2023-02-09 22:31:12 +0300

    I made you out of what I found in my backyard
    I made you out of what I found in my backyard

--- NewsPortal_D3.6/project/simpleapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category, NewsPortal
-
-
-admin.site.register(Category)
-admin.site.register(NewsPortal)
--- NewsPortal_D5/newsportal/news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Author)
